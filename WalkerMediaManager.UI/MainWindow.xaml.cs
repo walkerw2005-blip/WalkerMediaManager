e91af4f53b64e423d48686cbe269600e9bbdefc9
walkerw2005-blip <walkerw2005@gmail.com>
2026-07-20 18:37:45 -0600

    Sprint 1.3 - Add application navigation shell

--- WalkerMediaManager.UI/MainWindow.xaml.cs
+++ WalkerMediaManager.UI/MainWindow.xaml.cs
@@ -1,13 +1,61 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using WalkerMediaManager.UI.Views;
+using Windows.UI.ApplicationSettings;
 
-namespace WalkerMediaManager.UI
+namespace WalkerMediaManager.UI;
+
+public sealed partial class MainWindow : Window
 {
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        Title = "Walker Media Manager";
+
+        ContentFrame.Navigate(typeof(DashboardPage));
+    }
+
+    private void MainNavigationView_SelectionChanged(
+        NavigationView sender,
+        NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.IsSettingsSelected)
         {
-            InitializeComponent();
-            Title = "Walker Media Manager";
+            NavigateTo(typeof(SettingsPage));
+            return;
+        }
+
+        if (args.SelectedItemContainer is not NavigationViewItem selectedItem)
+        {
+            return;
+        }
+
+        string? tag = selectedItem.Tag?.ToString();
+
+        Type? destinationPage = tag switch
+        {
+            "dashboard" => typeof(DashboardPage),
+            "smartbuy" => typeof(SmartBuyPage),
+            "movies" => typeof(MoviesPage),
+            "tvshows" => typeof(TvShowsPage),
+            "collections" => typeof(CollectionsPage),
+            "wishlist" => typeof(WishlistPage),
+            "reports" => typeof(ReportsPage),
+            _ => null
+        };
+
+        if (destinationPage is not null)
+        {
+            NavigateTo(destinationPage);
+        }
+    }
+
+    private void NavigateTo(Type pageType)
+    {
+        if (ContentFrame.CurrentSourcePageType != pageType)
+        {
+            ContentFrame.Navigate(pageType);
         }
     }
 }
